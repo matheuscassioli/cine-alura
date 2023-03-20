424e992857f587d3f11547bc4913b119f26ea237
Matheus Cassioli <matheuscassioli@hotmail.com>
2023-03-20 20:26:14 -0300

    adicionando favoritos com redux

--- src/Componentes/Links/index.js
+++ src/Componentes/Links/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./Links.module.css"
 import link from "../../json/db.json"
 import "./Links.css"
@@ -9,25 +9,38 @@ export default function Links() {
 
     const dispatch = useDispatch();
 
+    const [favs, setFavs] = useState([])
+
     const changeBgFav = (e) => {
-        console.log(e.target.parentNode)
-        e.target.classList.toggle('favPreen')
-        dispatch({ type: SET_FAVS, favs:  e.target.parentNode})
+        e.target.classList.toggle('favPreen') 
+        let data = e.target.parentNode.getAttribute('data')
+        if (e.target.classList.contains('favPreen')) {
+            if (favs.hasOwnProperty(data)) {
+                console.log('sim')
+            } else {    
+                favs.push(data)
+            }
+            dispatch({ type: SET_FAVS, favs: favs })
+        } else{
+            var indice = favs.indexOf(data);
+            favs.splice(indice, 1)
+            dispatch({ type: SET_FAVS, favs: favs })
+        }
     }
 
     return (
         <div className={styles.links}>
             {link.map(key => {
-                return <div key={key.id} className={styles.link}>
-                        <div className={styles.img} >
-                            <img src={key.capa} className={styles.capa}></img>
-                            <h4 className={styles.titulo}>{key.titulo}</h4>
-                            <div className={styles.fav} onClick={(e) => changeBgFav(e)}>
-                            </div>
+                return <div key={key.id} className={styles.link} >
+                    <div className={styles.img} data={key.id}>
+                        <img src={key.capa} className={styles.capa}></img>
+                        <h4 className={styles.titulo}>{key.titulo}</h4>
+                        <div className={styles.fav} onClick={(e) => changeBgFav(e)}>
                         </div>
+                    </div>
                 </div>
             }
-            )}
+            )} 
         </div>
     )
 }
--- src/Pages/Favoritos/index.js
+++ src/Pages/Favoritos/index.js
@@ -1,12 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Banner from 'Componentes/Banner'
 import Titulo from "Componentes/Titulo"
+import store from 'store/store'
+import link from "../../json/db.json"
+import styles from "../../Componentes/Links/Links.module.css"
 
 export default function Favoritos() {
+
+    const [favs, setFavs] = useState({})
+
+    useEffect(() => {
+        let numbers = store.getState().favs
+
+        Object.keys(numbers == undefined ? '' : numbers).map(key => {
+            Object.keys(link).filter(k => {
+                if (link[k].id == numbers[key]) {
+                    setFavs(link[k])
+                }
+            })
+        })
+
+    }, [])
     return (
         <>
             <Banner imagem="2" />
             <Titulo children="Meus Favoritos" />
+            <div className={styles.links}>
+                {Object.keys(favs).map(key => {
+                    return <div key={favs.id+Math.random()} className={styles.link} >
+                        <div className={styles.img} data={favs.id}>
+                           <img src={favs.capa} className={styles.capa}></img>
+                             <h4 className={styles.titulo}>{favs.titulo}</h4>   
+                        </div>
+                    </div>
+                }
+                )}
+            </div>
         </>
     )
 }
