ad30ba1863526ea46eea8a7a54f3b5524ed95f3a
Matheus Cassioli <matheuscassioli@hotmail.com>
2023-03-20 20:49:28 -0300

    adicionando favoritos com redux 3

--- src/Componentes/Banner/index.js
+++ src/Componentes/Banner/index.js
@@ -3,7 +3,7 @@ import styles from "./Banner.module.css"
 export default function Banner({imagem}) { 
  
     return ( 
-        <div className={styles.banner} style={{backgroundImage: `url('/img/banner${imagem}.png')`}}>
+        <div className={styles.banner} style={{backgroundImage: `url('/Banner/banner${imagem}.png')`}}>
             
         </div> 
     )
